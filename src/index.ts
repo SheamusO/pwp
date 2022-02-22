e6928815006ba73f103f42ff43dc9cb45a903ff4
SheamusO <sohara4@cnm.edu>
2022-02-22 10:34:26 -0700

    recaptcha works, started adding better images

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import {Request, Response} from 'express'
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
-const Recaptcha = require('express-recaptcha').RecaptchaV3
+const Recaptcha = require('express-recaptcha').RecaptchaV2
 const formData = require('form-data')
 const Mailgun = require('mailgun.js')
 const mailgun = new Mailgun(formData)
